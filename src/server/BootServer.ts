b5a6963e6e1578c5e83aff3eb10fb4d27ed4197b
Marcin Galus <marcin.galus@ringieraxelspringer.com>
2025-07-09 10:14:40 +0200

    healthcheck test

--- src/server/BootServer.ts
+++ src/server/BootServer.ts
@@ -223,7 +223,7 @@ export class BootServer {
             // const usagePercent = (usedMemoryMB / totalMemoryMB) * 100;
 
             console.info(`Health check: free memory: ${freeMemMB}MB`);
-            if (freeMemMB < 100) {
+            if (freeMemMB < 800) {
                 return {
                     responseToReturn: new Response('Service Unavailable', { status: 503 })
                 };
